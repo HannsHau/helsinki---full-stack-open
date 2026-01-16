713c802c1b21aee02848caba09c5e0bf3ea60e48
HannsHau <bierkoenig@web.de>
2026-01-16 15:16:40 +0100

    1.2 Course Information, step 2

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -4,9 +4,19 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} ></Part>
+      <Part part={props.part2} exercise={props.exercise2} ></Part>
+      <Part part={props.part3} exercise={props.exercise3} ></Part>
+    </div>  
   )
 }
 
@@ -28,9 +38,8 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} 
+        part3={part3} exercise3={exercises3} />
       <Footer total={exercises1 + exercises2 + exercises3} />
     </>
   )
