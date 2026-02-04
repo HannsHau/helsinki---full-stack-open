f7ee81f091cd494761ab1b6487779c126de7f0b0
HannsHau <bierkoenig@web.de>
2026-02-04 20:53:24 +0100

    3.16: Phonebook database, step 4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -11,48 +11,47 @@ morgan.token('id', function getId (req) {
   return JSON.stringify(req.body)
 })
 
-//app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :id'))
 
-
-app.get('/api/persons', (request, response) => {
-    // response.json(persons)
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(notes => {
     response.json(notes)
   })
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
   const entries = persons.length
   const time = new Date()
   response.send(`<p>Phonebook has info for ${entries} people</p><p>${time.toString()}</p>`)
-
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
+  if (person) {
     response.json(person)
+  } else {
+    response.status(404).end()
+  }
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
+    return next('NameMissing')
   } else if (!body.number) {
-    return response.status(400).json({ 
-      error: 'number missing' 
-    })
+    return next('NumberMissing')
   }
 
   const person = new Person({
@@ -62,9 +61,31 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
+  })  
+  .catch(error => next(error))
 
 })
+
+const errorHandler = (error, request, response, next) => {
+
+  console.log('Error: ', error)
+
+
+  if (error === 'NameMissing') {
+    return response.status(400).send({ error: 'name missing' })
+  }
+  if (error === 'NumberMissing') {
+    return response.status(400).send({ error: 'Number is missing' })
+  } 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
