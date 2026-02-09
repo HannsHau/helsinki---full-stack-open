d010e12e6ac4476ff13e614315797ab47e4a8934
HannsHau <bierkoenig@web.de>
2026-02-09 12:08:04 +0100

    4.2 Blog List, step 2 separate index and app

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,20 +1,6 @@
-require('dotenv').config()
-
-const express = require('express')
-const mongoose = require('mongoose')
-
-const Blog = require('./models/blog')
-const logger = require('./utils/logger')
+const app = require('./app')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-const app = express()
-
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
