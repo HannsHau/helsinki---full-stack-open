13cbb3baae07476cb7e8fd631c7e3450acf1065b
HannsHau <bierkoenig@web.de>
2026-03-16 10:54:56 +0100

    prpare before 9.1 Body mass index

---
+++ part9/lecture/calculator.ts
@@ -0,0 +1,30 @@
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation) : number => {
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
+  }
+}
+
+console.log(process.argv)
+
+try {
+  console.log(calculator(1, 5 , 'divide'));
+  console.log(calculator(1, 1 , 'divide'));
+  console.log(calculator(1, 5 , 'add'));
+  console.log(calculator(1, 5 , 'multiply'));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
