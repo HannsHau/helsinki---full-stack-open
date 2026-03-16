880488fe6c998ae57565e5b03354eca23a0122c6
HannsHau <bierkoenig@web.de>
2026-03-16 11:12:27 +0100

    9.1 Body mass index

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const calculateBmi = (height: Number, weight: Number ) : String => {
+
+  const bmi = Number(weight) / (Number(height)/100 * Number(height)/100)
+
+  console.log('bmi: ', bmi)
+
+  if (bmi < 18.5) return 'Underweight'
+  if (bmi >= 18.5 && bmi < 25) return 'Normal Range'
+  if (bmi >= 25 && bmi <= 30) return 'Overweight'
+  if (bmi >= 30) return 'Obese'
+}
+
+
+console.log(calculateBmi(177, 78))
