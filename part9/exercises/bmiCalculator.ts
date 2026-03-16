6177d0ea81da3f7c190d1d56352afbc3eeae0981
HannsHau <bierkoenig@web.de>
2026-03-16 12:32:39 +0100

    9.3 Command line

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface BmiValue {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): BmiValue => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 const calculateBmi = (height: Number, weight: Number ) : String => {
 
   const bmi = Number(weight) / (Number(height)/100 * Number(height)/100)
@@ -10,5 +29,14 @@ const calculateBmi = (height: Number, weight: Number ) : String => {
   if (bmi >= 30) return 'Obese'
 }
 
-
-console.log(calculateBmi(177, 78))
+//console.log(calculateBmi(177, 78))
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
--- part9/exercises/exerciseCalculator.ts
+++ part9/exercises/exerciseCalculator.ts
@@ -1,41 +1,71 @@
-type Rating = 1 | 2 | 3;
+type Rating = 1 | 2 | 3
 
 interface Result {
-  periodLength: number;
-  trainingDays: number;
-  success: boolean;
-  rating: Rating;
-  ratingDescription: string;
-  target: number;
-  average: number;
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: Rating
+  ratingDescription: string
+  target: number
+  average: number
 }
 
-const getRating = (average: number, target: number) : Rating => {
-  if (average < target) {return 3} // failed
-  else if (average < target * 1.2) { return 2} // passed
-  else { return 1 } // excellent
-}  
+interface ExerciseValue {
+  hours: number[]
+  target: number
+}
 
-const getRatingText = (rating: Rating) : string => {
+const restValuesAreNum = (values: string[]) : boolean => {
+  const result = values.reduce((acc, cur) => acc && !isNaN(Number(cur)) , true)
+  return result
+}
+
+const parseArgumentsExercise = (args: string[]): ExerciseValue => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+
+  if (!isNaN(Number(args[2])) && restValuesAreNum(args.slice(3))) {
+    const hours = args.slice(3).map(v => Number(v))
+    const target = Number(args[2])
+    return {
+      hours,
+      target
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+const getRating = (average: number, target: number): Rating => {
+  if (average < target * 0.8) {
+    return 3
+  } // failed
+  else if (average < target * 1.2) {
+    return 2
+  } // passed
+  else {
+    return 1
+  } // excellent
+}
+
+const getRatingText = (rating: Rating): string => {
   switch (rating) {
     case 3:
-      return 'not too bad but could be better'
+      return 'failed, put more efford in'
     case 2:
-      return 'passed, you achived your target'
+      return 'not too bad but could be better'
     case 1:
       return 'excellent, you overarchived your target'
   }
-
 }
 
-const calculateExercises = (hours: number[], target: number) : Result => {
-
-  const periodLength : number = hours.length;
-  const trainingDays : number = hours.filter(h => h > 0).length;
-  const average : number = hours.reduce((acc, cur) => acc + cur, 0) / periodLength;
-  const success: boolean = average >= target;
-  const rating: Rating = getRating(average, target);
-  const ratingDescription = getRatingText(rating);
+const calculateExercises = (hours: number[], target: number): Result => {
+  const periodLength: number = hours.length
+  const trainingDays: number = hours.filter(h => h > 0).length
+  const average: number =
+    hours.reduce((acc, cur) => acc + cur, 0) / periodLength
+  const success: boolean = average >= target
+  const rating: Rating = getRating(average, target)
+  const ratingDescription = getRatingText(rating)
 
   return {
     periodLength,
@@ -46,7 +76,16 @@ const calculateExercises = (hours: number[], target: number) : Result => {
     target,
     average
   }
-
 }
 
-console.log('calculateExercises with [3, 0, 2, 4.5, 0, 3, 1] and 2: ', calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+//console.log('calculateExercises with [3, 0, 2, 4.5, 0, 3, 1] and 2: ', calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+  const { hours, target } = parseArgumentsExercise(process.argv)
+  console.log(calculateExercises(hours, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
