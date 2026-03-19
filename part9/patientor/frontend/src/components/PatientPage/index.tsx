4ce005441f163cad466cac29cfb3b898fd9bee10
HannsHau <bierkoenig@web.de>
2026-03-19 13:16:19 +0100

    9.24: Patientor, step 4

--- part9/patientor/frontend/src/components/PatientPage/index.tsx
+++ part9/patientor/frontend/src/components/PatientPage/index.tsx
@@ -1,8 +1,33 @@
-import { Patient, Gender } from "../../types";
-import { Container, Typography, Divider } from '@mui/material';
+import { Patient, Gender, Entry } from "../../types";
+import { Container, Typography, List, ListItem } from "@mui/material";
 import FemaleIcon from "@mui/icons-material/Female";
 import MaleIcon from "@mui/icons-material/Male";
 import TransgenderIcon from "@mui/icons-material/Transgender";
+
+interface EntriesProps {
+  entries: Entry[];
+}
+
+const Entries = ({ entries }: EntriesProps) => {
+  return (
+    <div>
+      {entries.map((e) => {
+        return (
+          <div>
+            <Typography>
+              {e.date} {e.description}
+            </Typography>
+            <List>
+            {e.diagnosisCodes?.map((code) => (
+              <ListItem>{code}</ListItem>
+            ))}
+            </List>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 interface Props {
   patient: Patient | undefined | null;
@@ -23,14 +48,21 @@ const getIcon = (gender: Gender) => {
 
 const PatientPage = ({ patient }: Props) => {
   if (!patient) return <>failed, no patient</>;
+
+  console.log("patient: ", patient);
+
   return (
     <Container>
       <Typography variant="h4" style={{ marginTop: "0.5em" }}>
         {patient.name} {getIcon(patient.gender)}
-      </Typography >
-      
-      <Typography >ssn: {patient.ssn}</Typography>
-      <Typography >occupation: {patient.occupation}</Typography >
+      </Typography>
+
+      <Typography>ssn: {patient.ssn}</Typography>
+      <Typography>occupation: {patient.occupation}</Typography>
+      <Typography variant="h5" style={{ marginTop: "0.5em" }}>
+        {patient.entries.length === 0 ? "no " : ""}entries
+      </Typography>
+      {patient.entries.length > 0 && <Entries entries={patient.entries} />}
     </Container>
   );
 };
--- part9/patientor/frontend/src/types.ts
+++ part9/patientor/frontend/src/types.ts
@@ -10,6 +10,12 @@ export enum Gender {
   Other = "other"
 }
 
+export interface Entry {
+  date: string;
+  description: string;
+  diagnosisCodes? : string[]
+}
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,6 +23,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
