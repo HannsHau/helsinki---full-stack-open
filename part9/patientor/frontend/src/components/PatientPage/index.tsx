652ab15d897236202d7e427e63f82780b3fccbc4
HannsHau <bierkoenig@web.de>
2026-03-19 10:36:54 +0100

    9.22: Patientor, step2

--- part9/patientor/frontend/src/components/PatientPage/index.tsx
+++ part9/patientor/frontend/src/components/PatientPage/index.tsx
@@ -1,7 +1,35 @@
-const PatientPage = () => {
+import { Patient, Gender } from "../../types";
+import FemaleIcon from "@mui/icons-material/Female";
+import MaleIcon from "@mui/icons-material/Male";
+import TransgenderIcon from "@mui/icons-material/Transgender";
+
+interface Props {
+  patient: Patient | undefined | null;
+}
+
+const getIcon = (gender: Gender) => {
+  switch (gender) {
+    case Gender.Male:
+      return <MaleIcon />;
+    case Gender.Female:
+      return <FemaleIcon />;
+    case Gender.Other:
+      return <TransgenderIcon />;
+    default:
+      return <>Gender not found</>;
+  }
+};
+
+const PatientPage = ({ patient }: Props) => {
+  if (!patient) return <>failed, no patient</>;
   return (
     <>
-      <h1>hello world</h1>
+      <h1>
+        {patient.name} {getIcon(patient.gender)}
+      </h1>
+
+      <p>ssh: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
     </>
   );
 };
